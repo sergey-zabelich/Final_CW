f5b9737a713f9ef78af352c2bde9e531ad34f73f
Sergey <zabelich19@gmail.com>
2023-08-05 23:54:36 +0300

    Написал код программы которая из имеющего массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array_1 = new string[4] { "Hello", "2", "world", ";)" };
+string[] array_2 = new string[array_1.Length];
+void SecArr(string[] array_1, string[] array_2)
+{
+    int count = 0;
+    for (int i = 0; i < array_1.Length; i++)
+    {
+        if (array_1[i].Length <= 3)
+        {
+            array_2[count] = array_1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecArr(array_1, array_2);
+PrintArray(array_2);
